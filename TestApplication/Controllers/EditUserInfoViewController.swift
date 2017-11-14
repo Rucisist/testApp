d29599cf217475883ec8d875bd3b47b11f6a9ef3
Rucisist <andreyilse@yandex.ru>
2017-11-14 10:40:42 +0300

    added scrollview

--- TestApplication/Controllers/EditUserInfoViewController.swift
+++ TestApplication/Controllers/EditUserInfoViewController.swift
@@ -14,6 +14,7 @@ class EditUserInfoViewController: UIViewController {
     @IBOutlet weak var emailLabel: UITextField!
     @IBOutlet weak var avatarURLLabel: UITextField!
     @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     enum buttonType {
         case add, edit
@@ -34,6 +35,22 @@ class EditUserInfoViewController: UIViewController {
 
         hideKeyboard()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Подписываемся на два уведомления, одно приходит при появлении клавиатуры
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name:
+            NSNotification.Name.UIKeyboardWillShow, object: nil)
+        // Второе когда она пропадает
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)),name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide,
+            object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -64,6 +81,25 @@ class EditUserInfoViewController: UIViewController {
             present(alert, animated: true, completion: nil)
         }
         
+    }
+    
+    
+    @objc func keyboardWasShown (notification : Notification ) {
+            // получаем размер клавиатуры
+            let info = notification.userInfo! as NSDictionary
+            let kbSize = (info.value(forKey: UIKeyboardFrameEndUserInfoKey ) as!NSValue ).cgRectValue.size
+            let contentInsets = UIEdgeInsetsMake (0.0, 0.0, kbSize.height, 0.0)
+            // добавляем отступ внизу UIScrollView равный размеру клавиатуры
+            self.scrollView?.contentInset = contentInsets
+            scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    
+    //когда клавиатура исчезает
+    @objc func keyboardWillBeHidden (notification: Notification ) {
+        // устанавливаем отступ внизу UIScrollView равный 0
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
     }
     
     func checkData(emailAdress: String, firstName: String, lastName: String) -> Bool {
